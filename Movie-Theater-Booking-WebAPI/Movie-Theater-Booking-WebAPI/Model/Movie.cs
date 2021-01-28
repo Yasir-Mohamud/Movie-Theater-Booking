edbbb7d5e56e1b1237f1f87031fe66f67a97f62a
yasir mohamud <ymohamed018@gmail.com>
2021-01-28 04:25:27 -0800

    commit

---
+++ Movie-Theater-Booking-WebAPI/Movie-Theater-Booking-WebAPI/Model/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Theater_Booking_WebAPI.Model
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Duration { get; set; }
+        public string Rating { get; set; }
+    }
+}
---
+++ Movie-Theater-Booking-WebAPI/Movie-Theater-Booking-WebAPI/Model/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Theater_Booking_WebAPI.Model
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public int Seats { get; set; }
+        public int RoomNumber { get; set; }
+        public int Floor { get; set; }
+    }
+}
