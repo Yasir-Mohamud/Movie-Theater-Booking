eb265ca1b812fdc0256940857663552e23f00f57
yasir mohamud <ymohamed018@gmail.com>
2021-02-14 16:58:02 -0800

    adds migration and updates database

---
+++ Movie-Theater-Booking-WebAPI/Movie-Theater-Booking-WebAPI/Data/TheaterDbContext.cs
@@ -0,0 +1,68 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Movie_Theater_Booking_WebAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Theater_Booking_WebAPI.Data
+{
+    public class TheaterDbContext : DbContext
+    {
+        public TheaterDbContext(DbContextOptions<TheaterDbContext> options) : base(options)
+        {
+
+        }
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    Id = 1,
+                    Name = "One Piece Stampede",
+                    Description = " Luffy finds the out about an old crew mate of the Pirate King Gol D. Roger, and has the eternal compass to the ancient land " +
+                    "Raftel, He has to over come this great foe in order to be closer to the greatest treasure , ONE PIECE",
+                    Duration = "1hr and 30 mins",
+                    Rating = "5 stars",
+
+
+                }
+                );
+
+            modelBuilder.Entity<Theater>().HasData(
+             new Theater
+             {
+                 Id = 1,
+                 Name = "The Colosseum",
+                 Address = " 123 sw ",
+                 City = "Seattle",
+                 State = "WA",
+                 Phone = "123456789",
+                 BusinessHours = " 9AM - 12AM",
+             }
+             );
+
+            modelBuilder.Entity<Room>().HasData(
+                new Room
+                {
+                    Id = 1,
+                    Seats = 50,
+                    RoomNumber = 12,
+                    Floor = 2,
+                }
+                );
+         }
+
+
+        // to create an intial migration
+        // add-migration intial
+        // Install-Package Microsoft.EntityFrameworkCore.Tools
+        // add-migration {migrationName}
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Theater> Theaters { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}           
--- Movie-Theater-Booking-WebAPI/Movie-Theater-Booking-WebAPI/Startup.cs
+++ Movie-Theater-Booking-WebAPI/Movie-Theater-Booking-WebAPI/Startup.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Movie_Theater_Booking_WebAPI.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,11 +28,21 @@ namespace Movie_Theater_Booking_WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
 
+            // Register with the app, that the database exists, and what options to use for it. 
+            services.AddDbContext<TheaterDbContext>(options =>
+            {
+                // Install-Package Microsoft.EntityFrameworkCore.SqlServer
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            // this is where all of our dependencies are going to live. 
+            // Enable the use of using controllers within the MVC convention
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Movie_Theater_Booking_WebAPI", Version = "v1" });
             });
+
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +63,7 @@ namespace Movie_Theater_Booking_WebAPI
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
