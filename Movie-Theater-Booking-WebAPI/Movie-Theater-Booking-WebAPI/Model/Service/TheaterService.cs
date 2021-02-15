b861aae95176b2bcf7a25eb2dc71250bf99ddcd9
yasir mohamud <ymohamed018@gmail.com>
2021-02-14 17:00:33 -0800

    adds services

---
+++ Movie-Theater-Booking-WebAPI/Movie-Theater-Booking-WebAPI/Model/Service/TheaterService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Theater_Booking_WebAPI.Model.Service
+{
+    public class TheaterService
+    {
+    }
+}
