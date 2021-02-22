2bd44579663e69cdaaf98972ec3cabfa0391da32
yasir mohamud <ymohamed018@gmail.com>
2021-02-22 04:20:10 -0800

    adds all controllers

---
+++ Movie-Theater-Booking-WebAPI/Movie-Theater-Booking-WebAPI/Controllers/RoomController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie_Theater_Booking_WebAPI.Model;
+using Movie_Theater_Booking_WebAPI.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Movie_Theater_Booking_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private readonly IRoom _room;
+
+        public RoomController(IRoom room)
+        {
+            _room = room;
+        }
+        // GET: api/<RoomController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Room>>> GetAll()
+        {
+            return await _room.GetAllRooms();
+        }
+
+        // GET api/<RoomController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Room>> Get(int id)
+        {
+            return await _room.GetRoom(id);
+        }
+
+        // POST api/<RoomController>
+        [HttpPost]
+        public async Task<ActionResult<Room>> Post(Room room)
+        {
+            await _room.CreateRoom(room);
+            return CreatedAtAction("GetRoom", new { id = room.Id }, room);
+        }
+
+        // PUT api/<RoomController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Room room)
+        {
+            if (id != room.Id)
+            {
+                return BadRequest();
+            }
+
+            var updatedRoom = await _room.UpdateRoom(room);
+
+            return Ok(updatedRoom);
+        }
+
+        // DELETE api/<RoomController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Room>> Delete(int id)
+        {
+            await _room.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Movie-Theater-Booking-WebAPI/Movie-Theater-Booking-WebAPI/Migrations/20210222121358_commit.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Movie_Theater_Booking_WebAPI.Migrations
+{
+    public partial class commit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RoomToMovies",
+                columns: table => new
+                {
+                    RoomId = table.Column<int>(type: "int", nullable: false),
+                    MovieId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomToMovies", x => new { x.RoomId, x.MovieId });
+                    table.ForeignKey(
+                        name: "FK_RoomToMovies_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoomToMovies_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TheaterToRooms",
+                columns: table => new
+                {
+                    TheaterId = table.Column<int>(type: "int", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TheaterToRooms", x => new { x.RoomId, x.TheaterId });
+                    table.ForeignKey(
+                        name: "FK_TheaterToRooms_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TheaterToRooms_Theaters_TheaterId",
+                        column: x => x.TheaterId,
+                        principalTable: "Theaters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomToMovies_MovieId",
+                table: "RoomToMovies",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TheaterToRooms_TheaterId",
+                table: "TheaterToRooms",
+                column: "TheaterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoomToMovies");
+
+            migrationBuilder.DropTable(
+                name: "TheaterToRooms");
+        }
+    }
+}
